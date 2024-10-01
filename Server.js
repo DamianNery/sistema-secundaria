73dfe2d5e80d397f403a2cbd0a726fb78ab90b7b
Damian <damian@email.com>
2024-09-30 22:21:59 -0300

    Métodos agregados a Server.js

--- Server.js
+++ Server.js
@@ -6,12 +6,27 @@ class Server {
     constructor() {
         this.port = process.env.PORT; //Puerto de escucha (default 3000)
         this.app = express(); //Instancia de express
+        this.cargarMiddlewares(); //Cargar middlewares
+        this.cargarRutas(); //Cargar rutas
+        this.conectarABD(); //Conectar a base de datos
     }
 
     listen() {
         this.app.listen(this.port, () => {
             console.log(`Servidor corriendo en puerto ${this.port}`); //Imprimir en consola
         }); //Iniciar el servidor
+    }
+
+    cargarMiddlewares() {
+        //PENDIENTE
+    }
+
+    cargarRutas() {
+        this.app.use("/api/ingredientes",require('./routes/ingredientes'));
+    }
+
+    conectarABD() {
+        //PENDIENTE
     }
 }
 
