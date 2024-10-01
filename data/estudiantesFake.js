f49409c92bee4ff4d5e4dd72de78f587614035b7
Damian <damian@email.com>
2024-10-01 09:34:10 -0300

    Agregar data fake estudiantes

---
+++ data/estudiantesFake.js
@@ -0,0 +1,32 @@
+module.exports = [
+    {
+        "_id": 1,
+        "nombre": "Luciana",
+        "apellido": "Fernandez",
+        "dni": "36925814"
+    },
+    {
+        "_id": 2,
+        "nombre": "Martin",
+        "apellido": "Pereyra",
+        "dni": "35768291"
+    },
+    {
+        "_id": 3,
+        "nombre": "Sofia",
+        "apellido": "Gonzalez",
+        "dni": "32165498"
+    },
+    {
+        "_id": 4,
+        "nombre": "Federico",
+        "apellido": "Acosta",
+        "dni": "38192764"
+    },
+    {
+        "_id": 5,
+        "nombre": "Camila",
+        "apellido": "Lopez",
+        "dni": "40750836"
+    }
+];
