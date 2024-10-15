8d1474ca9166a0545ac5a735b3ece98684e6835d
Damian <damian@email.com>
2024-10-15 09:18:27 -0300

    Pequeños ajustes

--- README.md
+++ README.md
@@ -1,6 +1,2 @@
 # sistema-secundaria
 2do parcial y final de Programación 3 - 2024
-
-# Para el Express rest server
-npm install express
-npm install dotenv
--- routes/estudiantes.js
+++ routes/estudiantes.js
@@ -9,12 +9,16 @@ const { getEstudiantes, getEstudiante,
 
 const {validarJwt, validarRol} = require('../middlewares/validations.js');//Cargar middlewares
 
-router.get('/', getEstudiantes); //Ruta para obtener todos los estudiantes
-//router.get('/:id', getEstudiante); //Ruta para obtener un estudiante por su id
-router.get('/:id', [validarJwt], getEstudiante); //Ruta para obtener un estudiante por su id
-router.post('/', postEstudiante); //Ruta para crear un estudiante
-router.put('/:id', updateEstudiante); //Ruta para actualizar un estudiante por su id
-//router.delete('/:id', deleteEstudiante); //Ruta para eliminar un estudiante por su id
-router.delete('/:id', [validarJwt, validarRol], deleteEstudiante); //Ruta para eliminar un estudiante por su id
+router.get('/', getEstudiantes); //Ruta PUBLICA para obtener todos los estudiantes
+
+//router.get('/:id', getEstudiante); //Ruta PUBLICA para obtener un estudiante por su id --> USAR PARA PRUEBAS
+router.get('/:id', [validarJwt], getEstudiante); //Ruta CON VALIDACIÓN para obtener un estudiante por su id
+
+router.post('/', postEstudiante); //Ruta PUBLICA para crear un estudiante
+
+router.put('/:id', updateEstudiante); //Ruta PUBLICA para actualizar un estudiante por su id
+
+//router.delete('/:id', deleteEstudiante); //Ruta para eliminar un estudiante por su id --> USAR PARA PRUEBAS
+router.delete('/:id', [validarJwt, validarRol], deleteEstudiante); //Ruta CON VALIDACIÓN para eliminar un estudiante por su id
 
 module.exports = router;
